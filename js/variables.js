8e3a920b259872f0d57ebb39bab8208b28f57a86
Héctor Aguirre <74071036+hearagmu@users.noreply.github.com>
2022-09-19 00:11:07 -0500

    API

--- js/variables.js
+++ js/variables.js
@@ -50,45 +50,24 @@ var use_default_map_style = false;              //If true, default map style wil
 //List of map markers
 var map_markers = [
     {
-        "title": "Reception",
-        "latitude": 33.775664,
-        "longitude": -118.065090,
+        "title": "Recepcion",
+        "latitude": 19.427758,
+        "longitude": -103.556559,
         "icon": "fas fa-glass-cheers", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-        "infoWindow": "Old Ranch Country Club <br> 29033 West Lake Drive, Agoura Hills, CA"
+        "infoWindow": "Rancho el Campanario, Tonila, Jalisco."
     },
     {
-        "title": "Accommodation 1",
-        "latitude": 33.777929,
-        "longitude": -118.076891,
+        "title": "Hotel",
+        "latitude": 19.392056, 
+        "longitude": -103.527794,
         "icon": "fas fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-        "infoWindow": "Accommodation 1 <br> Rossmoor, CA 90720"
+        "infoWindow": "Hacienda la Esperanza, Tonila, Jalisco."
     },
     {
-        "title": "Accommodation 2",
-        "latitude": 33.780428,
-        "longitude": -118.084075,
-        "icon": "fas fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-        "infoWindow": "Accommodation 2 <br> Los Alamitos, CA 90720"
-    },
-    {
-        "title": "Accommodation 3",
-        "latitude": 33.777551,
-        "longitude": -118.050468,
-        "icon": "fas fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-        "infoWindow": "Accommodation 3 <br> Seal Beach, CA 90740"
-    },
-    {
-        "title": "Transportation",
-        "latitude": 33.782930,
-        "longitude": -118.060552,
-        "icon": "fas fa-plane", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-        "infoWindow": "Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
-    },
-    {
-        "title": "Ceremony",
-        "latitude": 33.776384,
-        "longitude": -118.058149,
+        "title": "Ceremonia",
+        "latitude": 19.3876739,
+        "longitude": -103.571636,
         "icon": "fas fa-bell", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-        "infoWindow": "Birchwood Church <br> Seal Beach, CA 90740"
+        "infoWindow": "Templo de Nuestra Señora de la Merced, Quesería, Cuauhtémoc, Col."
     }
 ];
